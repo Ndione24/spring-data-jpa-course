8bee152b61b021e5468e61dc7519a50e1bdddd84
ndione <ndionebaba@gmail.com>
2022-07-26 11:17:18 +0200

    Crud action

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.repostory.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    @Autowired
+    private StudentRepository studentRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Crud Repo");
+        studentRepository.save(new Student("fai","li","fjf",12));
+        System.out.println(studentRepository.findAll());
+    }
 }
